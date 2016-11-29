a9caa7bd42e9bd15e5766ae8d4c263f6bfdfa28d
root <root@localhost.localdomain>
2016-11-30 01:35:23 +0530

    added the cli options

---
+++ src/options.js
@@ -0,0 +1,31 @@
+// This will store the options supported in command line.
+
+var options = [{
+        name: 'help',
+        abbr: 'h',
+        boolean: true,
+        help: 'Output usage information',
+    },
+    {
+        name: 'version',
+        abbr: 'v',
+        boolean: true,
+        help: 'Output the version of the tool'
+    },
+    {
+        name: 'input',
+        abbr: 'i',
+        boolean: true,
+        help: 'Input file name (Default file name is swagger.json)',
+        Default: './swagger.json'
+    },
+    {
+        name: 'output',
+        abbr: 'o',
+        boolean: true,
+        help: 'Output file name (Default file name is swagger.md)',
+        Default: './swagger.md'
+    }
+];
+
+module.exports = options;
